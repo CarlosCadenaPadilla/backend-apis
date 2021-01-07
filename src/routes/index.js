dd2c0471e2e53421d6d6f3215f6241d95507d544
Carlos Cadena <carlos.cadena@correounivalle.edu.co>
2021-01-07 09:36:44 -0500

    [ADD] fist steps add

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ require("express-async-errors"); //captura de errores de middleware
 const cors = require("cors");
 const helmet = require("helmet"); // seguridad
 const compression = require("compression"); //comprimir peticiones http rapidez
+const { ErrorMiddleware, NotFoundMiddleware } = require("../middlewares");
 
 module.exports = function({ HomeRoutes }) {
     const router = express.Router();
@@ -13,6 +14,9 @@ module.exports = function({ HomeRoutes }) {
     apiRoutes.use("/home", HomeRoutes);
 
     router.use("/v1/api", apiRoutes);
+
+    router.use(NotFoundMiddleware);
+    router.use(ErrorMiddleware);
 
     return router;
 };
